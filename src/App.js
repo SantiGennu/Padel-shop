982fb9323d5cdd188ad2b6df61853ce70bd11098
SantiGennu <santigennu5@gmail.com>
2022-08-30 09:34:40 +0200

    carpeta creada

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import NavBar from "./components/NavBar";
+import NavBar from "./components/NavBar/NavBar";
 import ItemListContainer from "./components/ItemListContainer";
 import "./app/styles.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
